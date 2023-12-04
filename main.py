5db07968ea298d4ae50e9108f88c6d2bcb6a85b1
Kasun Abeyweera <64094014+KasunAbeyweera@users.noreply.github.com>
2023-12-04 14:37:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# function to current date and time
+def current_date_time():
+    import datetime
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+# Main function
+
+if __name__ == '__main__':
+    print(current_date_time())
